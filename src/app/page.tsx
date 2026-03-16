d6b423288a50f402cf7ef04cc24745fc835a4abf
Kenan Uludağ <kuludagg@hotmail.com>
2026-03-16 15:31:26 +0300

    fix root redirect page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-Application error: a client-side exception has occurred while loading ai.merch.ebiidesign.com (see the browser console for more information).import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
   redirect("/dashboard/shops");
